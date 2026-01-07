88e5d43c97d155edbd3e72931789e9d407163188
Carlos Gonzalez <gonzalezcarlos7684@gmail.com>
2026-01-06 20:18:31 -0600

    ajuste de pase

--- fegus/src/environments/environment.prod.ts
+++ fegus/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.midominio.com/api'
+  apiUrl: 'https://apifegus-aygmgvgjhzakehb0.centralus-01.azurewebsites.net/deudores/200/707142127'
 };
