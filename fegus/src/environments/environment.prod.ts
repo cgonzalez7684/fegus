5cc1936f4a686d98bb663a771a68cf58296297b4
Carlos Gonzalez <gonzalezcarlos7684@gmail.com>
2026-01-11 23:29:31 -0600

    ajussssss

--- fegus/src/environments/environment.prod.ts
+++ fegus/src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
   AuthClientId: 'cb8fcede-1f85-4d07-b4f5-b17b205481a6', // 👈 Fegus SPA (Azure)
   AuthAuthority: 'https://login.microsoftonline.com/common',  
   ApiAZScope: 'api://6213113d-96b9-44a6-981a-10f4c542cf43/access_as_user', // 👈 Reemplaza con el Application ID URI de tu API
-  AuthRedirectUri: 'https://agreeable-pond-063aa5f10.2.azurestaticapps.net'
+  AuthRedirectUri: 'https://agreeable-pond-063aa5f10.2.azurestaticapps.net/dashboard'
 };
